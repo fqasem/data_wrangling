f5e9b29ce55c4de2ac7a2ed4d92d65e325f39d5e
fqasem <farrah.qasem@outlook.com>
2025-08-05 07:39:05 -0400

    Fixed code

--- data_wrangling_examples.py
+++ data_wrangling_examples.py
@@ -1,181 +1,159 @@
-## Data exploration in python
-
 # Import pandas package
 import pandas as pd
 
-# Assign data
-data = {'Name': ['Jai', 'Princi', 'Gaurav', 
-                 'Anuj', 'Ravi', 'Natasha', 'Riya'],
+def main():
+    ## Data exploration in python
+
+    # Assign data
+    data = {
+        'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj', 'Ravi', 'Natasha', 'Riya'],
         'Age': [17, 17, 18, 17, 18, 17, 17],
         'Gender': ['M', 'F', 'M', 'M', 'M', 'F', 'F'],
-        'Marks': [90, 76, 'NaN', 74, 65, 'NaN', 71]}
+        'Marks': [90, 76, 'NaN', 74, 65, 'NaN', 71]
+    }
 
-# Convert into DataFrame
-df = pd.DataFrame(data)
+    # Convert into DataFrame
+    df = pd.DataFrame(data)
 
-# Display data
-df
+    # Display data
+    print(df)
 
-## Dealing with missing values in Python
+    ## Dealing with missing values in Python
 
-# Compute average
-c = avg = 0
-for ele in df['Marks']:
-    if str(ele).isnumeric():
-        c += 1
-        avg += ele
-avg /= c
+    # Compute average
+    c = avg = 0
+    for ele in df['Marks']:
+        if str(ele).isnumeric():
+            c += 1
+            avg += ele
+    avg /= c
 
-# Replace missing values
-df = df.replace(to_replace="NaN",
-                value=avg)
+    # Replace missing values
+    #df = df.replace(to_replace="NaN", value=avg)
+    # Convert 'Marks' to numeric, forcing errors to NaN
+    df['Marks'] = pd.to_numeric(df['Marks'], errors='coerce')
+    # Replace NaN with the average
+    df['Marks'] = df['Marks'].fillna(avg)
 
-# Display data
-df
+    # Display data
+    print(df)
 
-## Data Replacing in Data Wrangling
-## in the GENDER column, we can replace the Gender column data by categorizing them into different numbers.
+    ## Data Replacing in Data Wrangling
+    # in the GENDER column, we can replace the Gender column data by categorizing them into different numbers.
 
-# Categorize gender
-df['Gender'] = df['Gender'].map({'M': 0, 
-                                 'F': 1, }).astype(float)
+    # Categorize gender
+    df['Gender'] = df['Gender'].map({'M': 0, 'F': 1}).astype(float)
 
-# Display data
-df
+    # Display data
+    print(df)
 
-##Filtering data in Data Wrangling
-## suppose there is a requirement for the details regarding name, gender, and marks of the top-scoring students. Here we need to remove some using the pandas slicing method in data wrangling from unwanted data.
+    ## Filtering data in Data Wrangling
+    # suppose there is a requirement for the details regarding name, gender, and marks of the top-scoring students.
+    # Here we need to remove some using the pandas slicing method in data wrangling from unwanted data.
 
-# Filter top scoring students
-df = df[df['Marks'] >= 75].copy()
+    # Filter top scoring students
+    df = df[df['Marks'] >= 75].copy()
 
-# Remove age column from filtered DataFrame
-df.drop('Age', axis=1, inplace=True)
+    # Remove age column from filtered DataFrame
+    df.drop('Age', axis=1, inplace=True)
 
-# Display data
-df
+    # Display data
+    print(df)
 
-## Data Wrangling Using Merge Operation:
+    ## Data Wrangling Using Merge Operation:
 
-# Import module
-import pandas as pd
+    # Creating Dataframe
+    details = pd.DataFrame({
+        'ID': [101, 102, 103, 104, 105, 106, 107, 108, 109, 110],
+        'NAME': ['Jagroop', 'Praveen', 'Harjot', 'Pooja', 'Rahul', 'Nikita',
+                 'Saurabh', 'Ayush', 'Dolly', "Mohit"],
+        'BRANCH': ['CSE', 'CSE', 'CSE', 'CSE', 'CSE', 'CSE', 'CSE', 'CSE', 'CSE', 'CSE']
+    })
 
-# Creating Dataframe
-details = pd.DataFrame({
-    'ID': [101, 102, 103, 104, 105, 
-           106, 107, 108, 109, 110],
-    'NAME': ['Jagroop', 'Praveen', 'Harjot', 
-             'Pooja', 'Rahul', 'Nikita',
-             'Saurabh', 'Ayush', 'Dolly', "Mohit"],
-    'BRANCH': ['CSE', 'CSE', 'CSE', 'CSE', 'CSE', 
-               'CSE', 'CSE', 'CSE', 'CSE', 'CSE']})
+    # Creating Dataframe
+    fees_status = pd.DataFrame({
+        'ID': [101, 102, 103, 104, 105, 106, 107, 108, 109, 110],
+        'PENDING': ['5000', '250', 'NIL', '9000', '15000', 'NIL', '4500', '1800', '250', 'NIL']
+    })
 
-# Creating Dataframe
-fees_status = pd.DataFrame(
-    {'ID': [101, 102, 103, 104, 105, 
-            106, 107, 108, 109, 110],
-     'PENDING': ['5000', '250', 'NIL', 
-                 '9000', '15000', 'NIL',
-                 '4500', '1800', '250', 'NIL']})
+    # Merging Dataframe
+    print(pd.merge(details, fees_status, on='ID'))
 
-# Merging Dataframe
-print(pd.merge(details, fees_status, on='ID'))
+    ## Data Wrangling Using Grouping Method 
+    # Use case: cars sold during the year 2010
 
+    # Creating Data
+    car_selling_data = {
+        'Brand': ['Maruti', 'Maruti', 'Maruti', 'Maruti', 'Hyundai', 'Hyundai',
+                  'Toyota', 'Mahindra', 'Mahindra', 'Ford', 'Toyota', 'Ford'],
+        'Year': [2010, 2011, 2009, 2013, 2010, 2011, 2011, 2010, 2013, 2010, 2010, 2011],
+        'Sold': [6, 7, 9, 8, 3, 5, 2, 8, 7, 2, 4, 2]
+    }
 
-## Data Wrangling Using Grouping Method 
-## Use case: cars sold during the year 2010
-# Import module
-import pandas as pd
+    # Creating Dataframe for Provided Data
+    df = pd.DataFrame(car_selling_data)
 
-# Creating Data
-car_selling_data = {'Brand': ['Maruti', 'Maruti', 'Maruti', 
-                              'Maruti', 'Hyundai', 'Hyundai', 
-                              'Toyota', 'Mahindra', 'Mahindra', 
-                              'Ford', 'Toyota', 'Ford'],
-                    'Year':  [2010, 2011, 2009, 2013, 
-                              2010, 2011, 2011, 2010, 
-                              2013, 2010, 2010, 2011],
-                    'Sold': [6, 7, 9, 8, 3, 5, 
-                             2, 8, 7, 2, 4, 2]}
+    # Group the data when year = 2010
+    grouped = df.groupby('Year')
+    print(grouped.get_group(2010))
 
-# Creating Dataframe for Provided Data
-df = pd.DataFrame(car_selling_data)
+    #############################
+    # Data Wrangling  by Removing Duplication
+    # Pandas duplicates() method helps us to remove duplicate values from Large Data.
+    # An important part of Data Wrangling is removing Duplicate values from the large data set.
 
-# Group the data when year = 2010
-grouped = df.groupby('Year')
-print(grouped.get_group(2010))
+    # initializing Data
+    student_data = {
+        'Name': ['Amit', 'Praveen', 'Jagroop', 'Rahul', 'Vishal', 'Suraj',
+                 'Rishab', 'Satyapal', 'Amit', 'Rahul', 'Praveen', 'Amit'],
+        'Roll_no': [23, 54, 29, 36, 59, 38, 12, 45, 34, 36, 54, 23],
+        'Email': ['xxxx@gmail.com', 'xxxxxx@gmail.com', 'xxxxxx@gmail.com', 'xx@gmail.com',
+                  'xxxx@gmail.com', 'xxxxx@gmail.com', 'xxxxx@gmail.com', 'xxxxx@gmail.com',
+                  'xxxxx@gmail.com', 'xxxxxx@gmail.com', 'xxxxxxxxxx@gmail.com', 'xxxxxxxxxx@gmail.com']
+    }
 
+    # creating dataframe
+    df = pd.DataFrame(student_data)
 
-#############################
-# Data Wrangling  by Removing Duplication
-# Pandas duplicates() method helps us to remove duplicate values from Large Data. An important part of Data Wrangling is removing Duplicate values from the large data set.
+    # Here df.duplicated() list duplicate  Entries in ROllno.
+    # So that ~(NOT) is placed in order to get non duplicate values.
+    non_duplicate = df[~df.duplicated('Roll_no')]
 
-# Syntax: DataFrame.duplicated(subset=None, keep='first')
+    # printing non-duplicate values
+    print(non_duplicate)
 
-# Here subset is the column value where we want to remove the Duplicate value.
+    ######################
+    ## Creating New Datasets Using the Concatenation of Two Datasets
+    ##########################
 
-# In keeping, we have 3 options :
+    # Define a dictionary containing employee data 
+    data1 = {
+        'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Age': [27, 24, 22, 32],
+        'Address': ['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'],
+        'Qualification': ['Msc', 'MA', 'MCA', 'Phd'],
+        'Mobile No': [97, 91, 58, 76]
+    }
 
-# if keep ='first' then the first value is marked as the original rest of all values if occur will be removed as it is considered duplicate.
-# if keep='last' then the last value is marked as the original rest the above same values will be removed as it is considered duplicate values.
-# if keep ='false' all the values which occur more than once will be removed as all are considered duplicate values.
-######################################
+    # Define a dictionary containing employee data 
+    data2 = {
+        'Name': ['Gaurav', 'Anuj', 'Dhiraj', 'Hitesh'],
+        'Age': [22, 32, 12, 52],
+        'Address': ['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'],
+        'Qualification': ['MCA', 'Phd', 'Bcom', 'B.hons'],
+        'Salary': [1000, 2000, 3000, 4000]
+    }
 
-# import module
-import pandas as pd
+    # Convert the dictionary into DataFrame  
+    df = pd.DataFrame(data1, index=[0, 1, 2, 3])
 
-# initializing Data
-student_data = {'Name': ['Amit', 'Praveen', 'Jagroop', 
-                         'Rahul', 'Vishal', 'Suraj', 
-                         'Rishab', 'Satyapal', 'Amit', 
-                         'Rahul', 'Praveen', 'Amit'],
+    # Convert the dictionary into DataFrame  
+    df1 = pd.DataFrame(data2, index=[2, 3, 6, 7])
 
-                'Roll_no': [23, 54, 29, 36, 59, 38, 
-                            12, 45, 34, 36, 54, 23],
-                'Email': ['xxxx@gmail.com', 'xxxxxx@gmail.com', 
-                          'xxxxxx@gmail.com', 'xx@gmail.com', 
-                          'xxxx@gmail.com', 'xxxxx@gmail.com', 
-                          'xxxxx@gmail.com', 'xxxxx@gmail.com', 
-                          'xxxxx@gmail.com', 'xxxxxx@gmail.com',
-                          'xxxxxxxxxx@gmail.com', 'xxxxxxxxxx@gmail.com']}
-
-# creating dataframe
-df = pd.DataFrame(student_data)
-
-# Here df.duplicated() list duplicate  Entries in ROllno.
-# So that ~(NOT) is placed in order to get non duplicate values.
-non_duplicate = df[~df.duplicated('Roll_no')]
-
-# printing non-duplicate values
-print(non_duplicate)
-
-
-
-######################
-## Creating New Datasets Using the Concatenation of Two Datasets
-##########################
-
-# importing pandas module
-import pandas as pd 
-  
-# Define a dictionary containing employee data 
-data1 = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
-        'Age':[27, 24, 22, 32], 
-        'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'], 
-        'Qualification':['Msc', 'MA', 'MCA', 'Phd'],
-        'Mobile No': [97, 91, 58, 76]} 
+    res = pd.concat([df, df1])
     
-# Define a dictionary containing employee data 
-data2 = {'Name':['Gaurav', 'Anuj', 'Dhiraj', 'Hitesh'], 
-        'Age':[22, 32, 12, 52], 
-        'Address':['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'], 
-        'Qualification':['MCA', 'Phd', 'Bcom', 'B.hons'],
-        'Salary':[1000, 2000, 3000, 4000]} 
-  
-# Convert the dictionary into DataFrame  
-df = pd.DataFrame(data1,index=[0, 1, 2, 3])
-  
-# Convert the dictionary into DataFrame  
-df1 = pd.DataFrame(data2, index=[2, 3, 6, 7])
+    # Display the concatenated DataFrame
+    print(res)
 
-res = pd.concat([df, df1])
+if __name__ == "__main__":
+    main()
